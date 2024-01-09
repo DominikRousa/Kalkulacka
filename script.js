4d22437d091685aaead82dd81e0ae272e31cc4d6
DominikRousa <145441116+DominikRousa@users.noreply.github.com>
2024-01-09 12:59:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function addToCalc(value){
+    document.getElementById('output').value += value;
+}
+function resultCalc(){
+    var input = document.getElementById('output').value;
+    document.getElementById('output').value = eval(input)
+}
+function clearCalc(){
+    document.getElementById('output').value = '';
+}
